aaedffe5bc237c0d7f1812295a86ebbac5b65034
Suramack <sureshm2suresh@gmail.com>
2021-08-25 13:11:40 +0530

    hello world flutter

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Myapp());
+
+class Myapp extends StatelessWidget {
+  // const Myapp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return (MaterialApp(
+      title: 'Hello World',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello World'),
+        ),
+        body: Center(
+          child: Text("Hello World"),
+        ),
+      ),
+    ));
+  }
+}
